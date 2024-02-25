8e2351bffca10c3435cf934483ce30e3d07a5b32
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-02-25 10:52:13 +0300

    Create 2lab_without_functions.py

---
+++ 2lab_without_functions.py
@@ -0,0 +1,46 @@
+import random
+def funcount(a=[3],b=[5]):
+    counter=0
+    for i in  range(0,3):
+        for j in range (0,5):
+            if int(a[i])==int(b[j]):
+                counter+=1
+    return counter
+n = int(input())
+counter=0
+randomnumber = []
+answer=[]
+randomnumber.append(random.randint(0,n-4))
+for i in range(1, 3):
+    randomnumber.append(randomnumber[i-1]+2)
+randomnumber.sort()
+for i in range(0, 5):
+    answer.append(input("введите " + str(i) + "е чило"))
+    if answer[i] == "HELP":
+        print(randomnumber)
+answer.sort()
+for i in range(0, 3):
+    for j in range(0, 5):
+        if randomnumber[i] == int(answer[j]):
+            counter += 1
+if counter == 3:
+    print("YES")
+else:
+    print("NO")
+flag = False
+while flag == False :
+    for i in range(0, 5):
+        answer[i]=input("введите "+str(i)+"е чило   ")
+        if answer[i]=="HELP":
+            print(randomnumber)
+            flag=True
+            break
+    if(flag==True):break
+    answer.sort()
+    for i in  range(0,3):
+        for j in range (0,5):
+            if randomnumber[i]==int(answer[j]):
+                counter+=1
+    if counter==3:
+        print("YES")
+    else: print("NO")
